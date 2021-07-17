b1b4d00b81e22d34ada5048ca257608530d553a9
robertj1029 <robertj9216@gmail.com>
2021-07-17 21:54:12 +0900

    fixing the responsive issue

--- src/school/school.php
+++ src/school/school.php
@@ -101,30 +101,32 @@
     }
 </style>
 <script src="src/school/school.js"></script>
-<div class="w-100 h-75">
-    <div class="d-flex">
-        <img class="ml-auto mr-5 back-map-img" src="assets/images/back-map.png" onclick="goToURL('mainpage/mainpage.php')"/>
-    </div>
-    <div class="row">
-        <div class="col-md-5 col-sm-5">
-            <div>
-                <img class="school-image" src="assets/images/school.png" />
-            </div>
+<div class="w-100 h-100 d-flex align-items-center">
+    <div class="w-100">
+        <div class="d-flex">
+            <img class="ml-auto mr-5 back-map-img" src="assets/images/back-map.png" onclick="goToURL('mainpage/mainpage.php')"/>
         </div>
-        <div class="col-md-7 col-sm-7" align="center">
-            <img class="school-room-img" src="assets/images/SCHOOL---Room.png"/>
-            <a class="cursor-default" href="assets/images/SCHOOL-Desk.JPG" target="blank">
-                <div class="desk-rect"></div>
-            </a>
-            <a class="cursor-default" href="assets/images/SCHOOL-Trash.jpg" target="blank">
-                <div class="trash-bin-rect"></div>
-            </a>
-            <div class="eraser-rect" onclick="clickEraser()">
-                <img class="eraser" src="assets/images/eraser.png"/>
+        <div class="row">
+            <div class="col-md-5 col-sm-5">
+                <div>
+                    <img class="school-image" src="assets/images/school.png" />
+                </div>
             </div>
-            <a class="cursor-default" onclick="goToURL('school/phone.php')">
-                <div class="phone-rect"></div>
-            </a>
+            <div class="col-md-7 col-sm-7" align="center">
+                <img class="school-room-img" src="assets/images/SCHOOL---Room.png"/>
+                <a class="cursor-default" href="assets/images/SCHOOL-Desk.JPG" target="blank">
+                    <div class="desk-rect"></div>
+                </a>
+                <a class="cursor-default" href="assets/images/SCHOOL-Trash.jpg" target="blank">
+                    <div class="trash-bin-rect"></div>
+                </a>
+                <div class="eraser-rect" onclick="clickEraser()">
+                    <img class="eraser" src="assets/images/eraser.png"/>
+                </div>
+                <a class="cursor-default" onclick="goToURL('school/phone.php')">
+                    <div class="phone-rect"></div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
